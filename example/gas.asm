6bcd2003e5ddbc14022efd47607f16cbe15922aa
ScriptScorpion <maksim.09.09@bk.ru>
2025-07-25 14:51:36 +0300

    Added example for GAS compiler

---
+++ example/gas.asm
@@ -0,0 +1,18 @@
+# this example file for amd64 devices
+.section .data
+	hello:     .ascii "Hello world!\n"  
+	helloLen = . - hello            
+
+.section .text
+	.global _start
+
+_start:
+	mov $1, %rax          # 'write' system call = 4 in 32bit, in 64 bit Arch call=1
+	mov $1, %rdi          # file descriptor 1 = STDOUT
+	mov $hello, %rsi      # string to write
+	mov $helloLen, %rdx   # length of string to write
+	syscall               # call the kernel
+
+	mov $60, %rax         # 'exit' system call
+	xor %rdi, %rdi        # exit with error code 0
+	syscall               # call the kernel
