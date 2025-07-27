4f826846a20025ff149199ca4ac5252b5b1063eb
ScriptScorpion <maksim.09.09@bk.ru>
2025-07-27 15:28:58 +0300

    Big release, fixed issue with "-o"

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <cstring>
+#include <vector>
 
 #if __unix__
     const std::string OS =  "-f elf64 ";
 #else 
     throw std::runtime_error("Unsupported OS");
 #endif
-
 
 std::string removextension(const std::string &filename) {
     size_t lastDot = filename.find_last_of('.');
@@ -18,52 +19,49 @@ std::string removextension(const std::string &filename) {
     return filename; 
 }
 
-std::string Find(const std::string input) {
-    size_t pos = 0;
-    while (true) {
-        pos = input.find("-o", pos);
-        
-        bool isStandalone = true;
-        if (pos > 0 && !std::isspace(input[pos - 1])) {
-            isStandalone = false;
-        }
+bool ishavextension(const std::string &name) {
+    std::filesystem::path p(name);
+    return p.has_extension();
+}
 
-        if (isStandalone) {
-            pos += 2;
-            while (pos < input.size() && std::isspace(input[pos])) {
-                pos++;
-            }
-            if (pos < input.size()) {
-                return input.substr(pos);
+std::string Findvector(const std::vector<std::string>& vec) {
+    for (size_t i = 0; i < vec.size(); ++i) {
+        if (vec[i] == "-o") {
+            if (i + 1 < vec.size()) {  
+                return vec[i + 1];     
             } else {
-                return ""; 
+                return "";  
             }
-        } else {
-            return "";
         }
     }
+    return "";  
 }
 
 
 int main(int argc, char *argv[]) {
-
-    std::string arguments = "nasm " + OS;
+    
+    const std::string compiler = "nasm "; // change to your ASM compiler if it is GAS remove 8-12 lines
+    std::string arguments = compiler + OS;
     std::string linker= "ld "; 
     std::string exestr = "./";
-
-    for (int i = 1; i < argc; ++i) {
-        arguments += argv[i];
-        arguments += " ";
+    std::vector <std::string> safeargc(argv, argv + argc);
+    std::string Vec_extrac = Findvector(safeargc);  // gets file that specified by -o for vectors
+    if (argc == 1) {
+        std::cout << "Specify assembler file \n";
+        std::exit(1);
     }
-    std::string extrac = Find(arguments); // gets file that specified by -o
-    if (extrac.empty()) {
-        if (argc > 2) {
-            std::cout << "Specify assembler file \n";
-            std::exit(1);
+    for (int i = 1; i < argc; ++i) {   
+        if (safeargc[i] == "-o") {
+            if (!(Vec_extrac.empty()) && argc == 4) {
+                safeargc[3] += ".o";
+            }
+            else if (!(Vec_extrac.empty()) && argc > 4) {
+                break;
+                std::cout << "Sorry, you cannot have more then 4 arguments(I will fix this in next updates)";
+            }
         }
-        else {
-            extrac = argv[1];
-        }
+        arguments += safeargc[i];
+        arguments += " ";
     }
 
     int ASMoutcome = system(arguments.c_str());
@@ -71,21 +69,23 @@ int main(int argc, char *argv[]) {
         std::cout << "Compiling error \n";
         std::exit(1);
     }
+    if (Vec_extrac.empty()) {
+        Vec_extrac = argv[1];
+    }
+    Vec_extrac = removextension(Vec_extrac);
 
-    extrac = removextension(extrac);
-
-    linker += extrac;
+    linker += Vec_extrac;
     linker += ".o ";
     linker += "-o "; 
-    linker += extrac;
-
+    linker += Vec_extrac;
+    
     int Oresult = system(linker.c_str());
     if (Oresult != 0) {
         std::cout << "Linking Error \n";
         std::exit(1);
     }
 
-    exestr += extrac;
+    exestr += Vec_extrac;
     int exeresult = system(exestr.c_str());
     if (exeresult != 0) {
         std::cout << "Execution error \n";
