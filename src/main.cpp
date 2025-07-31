0d6e1aca44aa8bf68d4e6e7e3e3cb7d0398ef8b2
ScriptScorpion <maksim.09.09@bk.ru>
2025-07-31 18:29:12 +0300

    Added better compiling options for NASM and YASM compilers

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,10 @@
 #include <string>
 #include <cstring>
 #include <vector>
+#include <fstream>
 
 #if defined(__unix__) || defined(unix)
-    #if defined(__i386__) || (UINTPTR_MAX == 0xFFFFFFFF)
-        const std::string OS =  "-f elf32 ";
-        const std::string LD = "-m elf_i386 ";
-    #elif  defined(__x86_64__) || (UINTPTR_MAX == 0xFFFFFFFFFFFFFFFF)
-        const std::string OS = "-f elf64 ";
-        const std::string LD = "";
-    #endif
+    const std::string OS = "-f elf64 ";
 #else 
     throw std::runtime_error("Unsupported OS");
 #endif
@@ -67,6 +62,11 @@ int main(int argc, char *argv[]) {
     std::string arguments = "";
     std::string linker= "ld "; 
     std::string exestr = "./";
+    std::ifstream CodeArch;
+    std::string Arch = "";
+    std::string line;
+    std::string extraLd = "";
+    std::string FileOpen = argv[2];
     std::vector <std::string> safeargc(argv, argv + argc);
     std::string Vec_extrac = Findvector(safeargc);  // gets file that specified by -o for vectors
 
@@ -95,11 +95,23 @@ int main(int argc, char *argv[]) {
             return 1;
         }
     }
-    
+    CodeArch.open(FileOpen);
     for (auto x : Compilers) {
         if (argv[1] == Compilers[0]) {
             compiler = "nasm ";
-            arguments = compiler + OS;
+            arguments = compiler;
+            while (std::getline(CodeArch, line))
+            {
+                if (line.find("bits 32")) {
+                    arguments += "-f elf32 ";
+                    extraLd = "-m elf_i386 ";
+                    break;
+                }
+                else {
+                    arguments += OS;
+                    break;
+                }
+            }
             break;
         }
         else if (argv[1] == Compilers[1]) {
@@ -109,10 +121,23 @@ int main(int argc, char *argv[]) {
         }
         else if (argv[1] == Compilers[2]) {
             compiler = "yasm ";
-            arguments = compiler + OS;
+            arguments = compiler;
+            while (std::getline(CodeArch, line))
+            {
+                if (line.find("bits 32")) {
+                    arguments += "-f elf32 ";
+                    extraLd = "-m elf_i386 ";
+                    break;
+                }
+                else {
+                    arguments += OS;
+                    break;
+                }
+            }
             break;
         }
     }
+    CodeArch.close();
     for (int i = 2; i < argc; ++i) {
         if (safeargc[i] == "-o") {
             if (!(Vec_extrac.empty()) && argc == 5) {
@@ -128,15 +153,14 @@ int main(int argc, char *argv[]) {
         std::exit(1);
     }
     if (Vec_extrac.empty()) {
-        Vec_extrac = argv[2];
+        Vec_extrac = argv[2];   
     }
     Vec_extrac = removextension(Vec_extrac);
-    linker += LD;
+    linker += extraLd;
     linker += Vec_extrac;
     linker += ".o ";
     linker += "-o "; 
     linker += Vec_extrac;
-
     int Oresult = system(linker.c_str());
     if (Oresult != 0) {
         std::cerr << "Linking Error \n";
