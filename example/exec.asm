24ef3de144cfac2ba330672280855f1e056525c2
ScriptScorpion <63147222+ScriptScorpion@users.noreply.github.com>
2026-02-18 23:45:46 +0300

    exec function added

---
+++ example/exec.asm
@@ -0,0 +1,14 @@
+section .data
+    string: db "/bin/sh", 0
+section .text
+    global _start
+_start:
+   mov rax, 0x3b
+   mov rdi, string
+   mov rsi, 0
+   mov rdx, 0
+   syscall
+   
+   mov rax, 60
+   xor rdi, rdi
+   syscall
